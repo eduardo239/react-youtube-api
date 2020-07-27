4e34b1fb8eafb3d7ad0843d0fa05742ecda4d3b0
eduardo239 <eucrieiumaconta@gmail.com>
2020-07-27 12:12:25 -0300

    Updates

---
+++ precache-manifest.27f4eb91e6097b329b87e93ed6d6ebe3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dfffd84331acc773fec066a2ca0c2d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "649851a42f096012650a",
+    "url": "/static/css/main.c7e5cc45.chunk.css"
+  },
+  {
+    "revision": "653cfa9b9ea9d32f7076",
+    "url": "/static/js/2.d154fc14.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/static/js/2.d154fc14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "649851a42f096012650a",
+    "url": "/static/js/main.28d7463a.chunk.js"
+  },
+  {
+    "revision": "8c0ed676e7fc0e04cf11",
+    "url": "/static/js/runtime-main.81dc9e71.js"
+  }
+]);
